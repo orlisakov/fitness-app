357e5e09b607e8f939364bdabdca083c96d15d6a
אורלי איסקוב <orlisakov1@gmail.com>
2025-10-21 16:32:18 +0300

    תיקון תקפיט

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 // fitmatch/client/src/config.js
 const URL_WEB = "https://fitness-app-wdsh.onrender.com";
-const URL_local = "http://localhost:5000";
+//const URL_local = "http://localhost:5000";
 
 const config = {
   apiBaseUrl: URL_WEB,
